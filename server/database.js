f61b57c14190560ee760c04957929e5f80551bcf
Robert McLean <robertmcleanh@gmail.com>
2021-07-17 00:25:25 -0400

    Add server uri variable

--- server/database.js
+++ server/database.js
@@ -1,6 +1,10 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://robertmclean:NXQ4nTRzD3NMfLK@cluster0.brtqw.mongodb.net/personalSiteData?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
-
+const mongoose = require("mongoose");
+const connection = process.env.MONGO_URI;
+mongoose
+  .connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log(err));
